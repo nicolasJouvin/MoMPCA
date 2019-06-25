fac5f5893aeee36bed5f7cecd6b4b21cfc1ab302
nicolasJouvin <nicolasJouvin>
2019-06-25 19:26:36 +0200

    rewrite to take into account S4 programming. Now take a model argument and return an mmpca_clust object.

--- R/branch_and_bound_cvem.R
+++ R/branch_and_bound_cvem.R
@@ -1,53 +1,73 @@
-mmpca_clust <- function(dtm, Q, K,
-                             Yinit='random',
-                             init.beta=NULL,
-                             keep=-1,
-                             control_lda_loop=NULL,
-                             control_lda_init=NULL,
-                             max.iter=100,
-                             verbose=1) {
+mmpca_clust <- function(dtm,
+                        Q,
+                        K,
+                        model = NULL,
+                        Yinit = 'random',
+                        method = 'CVEM',
+                        init.beta = 'lda',
+                        keep = -1L,
+                        max.epochs = 100L,
+                        verbose = 1L) {
 
+  if (as.integer(K) != K || as.integer(K) < 2) stop("'K' needs to be an integer of at least 2")
+  if (as.integer(Q) != Q || as.integer(Q) < 2) stop("'Q' needs to be an integer of at least 2")
 
-  if (is.null(control_lda_init)) control_lda_init = default_control_init()
-  if (is.null(control_lda_loop)) control_lda_loop = default_control_loop()
-
-  N = dim(dtm)[1]
-  # V = dim(dtm)[2]
-  alpha = control_lda_loop$alpha
-
-  ###########################################
-  #' Initialisation of Y
-  #'
-  #' Several benchmarks possible.
-  ###########################################
-
-  init_method = 'user provided'
-  if (isString(Yinit)) {
-    init_method <- Yinit
-    Yinit <- initialize_Y(dtm, Q, K, init)
-  } else if (!is.vector(Yinit) || !is.matrix(Yinit)) {
-    stop('Yinit must be either a user-given clustering or a string specifying the initialization method')
+  mycall = match.call()
+  if (is.null(model)) {
+    model <- new("mmpca")
   }
+
+  control_lda_init = model@control_lda_init
+  control_lda_loop = model@control_lda_loop
+
+  if (control_lda_loop@alpha != control_lda_init@alpha)
+    warning("Two hyper-parameters alpha")
+
+  alpha = control_lda_loop@alpha
+  N = dim(dtm)[1]
+  V = dim(dtm)[2]
+
+  ###########################################
+  ## Initialisation of Y
+  ##
+  ## Several benchmarks possible.
+  ###########################################
+  init_method = 'user provided'
+  if (is.character(Yinit) && length(Yinit) == 1) {
+    init_method <- Yinit
+    Yinit <- initialize_Y(dtm, Q, K, init = init_method)
+  } else if (!is.vector(Yinit) && !is.matrix(Yinit)) {
+    stop('Yinit must be either a user-given clustering or a string
+         specifying the initialization method.')
+  } else if (length(Yinit) != N) {
+    stop(paste0("Yinit must be a vector of length ", N,
+                ' (length(Yinit) = ', length(Yinit), ').'))
+  } else if (length(unique(Yinit)) != Q) {
+    stop(paste0("Yinit must be a clustering with ", Q,
+                ' groups, not ', length(unique(Yinit)), '.'))
+  }
+
   Y <- Yinit
 
-
   ###################################################
-  #' ------ Different initialization for beta ---------
-  #'
-  #' * User defined : in this case init.beta is a user defined init matrix
-  #'
-  #' * 'random' : beta_ij = 1/V + U([0,1e-10])
-  #'
-  #' * 'lda' : LDA on full dtm. Succession of
-  #'           1. LDA with giibs : 5 restarts
-  #'           2. LDA with C-VEM
-  #'           hyper-parameter alpha is handeled carefully in the controls.
-  #'
-  #' * 'nmf' : take the basis of the NMF decomposition of the full dtm.
+  ## ------ Different initialization for beta ---------
+  ##
+  ## * User defined : in this case init.beta is a user
+  ##                  defined init matrix, of dim (KxV),
+  ##                  which rows sums to 1
+  ##
+  ## * 'random' : beta_ij = 1/V + U([0,1e-10])
+  ##
+  ## * 'lda' : LDA on full dtm. Succession of
+  ##           1. LDA with giibs : 5 restarts
+  ##           2. LDA with C-VEM
+  ##           hyper-parameter alpha is handeled carefully in the controls.
+  ##
+  ## * 'nmf' : take the basis of the NMF decomposition of the full dtm.
   ###################################################
 
   ## dummy topicmodels::lda object to store beta init
-  lda_init = topicmodel::LDA(dtm, k = K,
+  lda_init = topicmodels::LDA(dtm, k = K,
                              method = 'VEM',
                              control = list(estimate.alpha = FALSE,
                                             estimate.beta = FALSE,
@@ -59,13 +79,17 @@ mmpca_clust <- function(dtm, Q, K,
                                             )
                              )
 
-  ## initialize the la_init@beta slot
+  ## initialize the lda_init@beta slot
   if (is.matrix(init.beta) ) {
-    if (verbose > 0) cat('Beta initialisation with custom user given beta.\n')
+    if (!identical(dim(init.beta), c(K, V)) || identical(Matrix::rowSums(init.beta), rep(1, K))) {
+      stop('init.beta must be a KxV matrix which rows sums to 1.')
+    }
+
+    if (verbose > 0) cat('Beta initialisation with a user given beta.\n')
     lda_init@beta = log(init.beta)
-  } else if (isString(init.beta)) {
+  } else if (is.character(init.beta) && length(init.beta) == 1) {
     if (verbose > 0) cat('Beta initialisation with ',init.beta, '...\n')
-    beta.init = initializeBeta(dtm, init.beta, K, verbose, control_gibbs_init, control_lda_init)
+    beta.init = initializeBeta(dtm, init.beta, K, verbose, control_lda_init)
     lda_init@beta = log(beta.init)
     if (verbose > 0) cat('... Finished. \n')
   } else {
@@ -73,15 +97,15 @@ mmpca_clust <- function(dtm, Q, K,
   }
 
   ###########################################
-  #' Compute the bounds with topicmodels
-  #'
-  #' Only the var step is done, control
-  #' estimate.beta=F ensures the M-step is
-  #' locked, hence not re-restimation of beta
+  ## Compute the bounds with topicmodels
+  ##
+  ## Only the var step is done, control
+  ## estimate.beta=F ensures the M-step is
+  ## locked, hence not re-restimation of beta
   ############################################
 
   ## Construct meta observations
-  P = t(sparseMatrix(i = 1:N, j = Y, x = rep(1,N)) )
+  P = Matrix::t(Matrix::sparseMatrix(i = 1:N, j = Y, x = rep(1,N)))
   dtm_init = slam::as.simple_triplet_matrix(P %*% DTMtoSparse(dtm))
 
   ## An LDA to get the individual meta obervations bounds
@@ -90,41 +114,45 @@ mmpca_clust <- function(dtm, Q, K,
                  k = K,
                   control = list(estimate.alpha = FALSE,
                                  estimate.beta = F,
-                                 var = list(iter.max = control_lda_loop$var$iter.max,
-                                          tol = control_lda_loop$var$tol),
+                                 var = list(iter.max = control_lda_loop@var@iter.max,
+                                          tol = control_lda_loop@var@tol),
                                  keep = 1),
                  model = lda_init)
 
-  ## should not change but just in case for bound equalities throughout the rest.
-  beta.init = t(exp(lda_aggr@beta) / rowSums(exp(lda_aggr@beta)))
-  lbeta.init = t(lda_aggr@beta)
 
   ###################################################
-  #' ------ Branch & Bound with topicmodels ---------
-  #'
-  #'
-  #'   1 loop with beta fixed = B&B
-  #'      1 loop over each observation = test each swaps
-  #'         1 loop over q = VE-step to obtain the bound modification
+  ## ------ Branch & Bound with topicmodels ---------
+  ##
+  ##
+  ##   1 loop with beta fixed = B&B
+  ##      1 loop over each observation = test each swaps
+  ##         1 loop over q = VE-step to obtain the bound modification
   ###################################################
 
   ## Setup quantities used in the B&B
+  csize = table(Y)
+  PI = csize/N
   lda_algo = lda_aggr
-  estimate.beta.algo = control_lda_loop$estimate.beta
-  lbeta = lbeta.init
+  estimate.beta.algo = control_lda_loop@estimate.beta
   ite_cpt = 0
+  # individual meta-observations' contribution to bound
+  meta_bounds = rep(0,Q)
+  for (q in 1:Q) {
+    meta_bounds[q] = lda_algo@loglikelihood[q] + csize[q]*log(PI[q])
+  }
+  current_bound = sum(meta_bounds)
 
   ## stock the evolution of the bound every keep iterations
   bounds = c()
 
   ## for debugging : to keep track of the conservation of sum(Vgammas)
-  theoretical.gammaSum = Q*K*alpha + sum(dtm.full)
+  # theoretical.gammaSum = Q*K*alpha + sum(dtm)
 
   if (verbose > 1) cat(' ---- Begin B&B on ', N, ' documents. ----\n')
-  for (ite in 1:max.iter) {
+  for (epoch in 1:max.epochs) {
 
     if (verbose > 1) {
-      cat('\n ---------- Epoch number ', ite, ' : \n ')
+      cat('\n ---------- Epoch number ', epoch, ' : \n ')
     }
 
     # M-step on $\pi$.
@@ -134,6 +162,10 @@ mmpca_clust <- function(dtm, Q, K,
     # optimisation of Y : greedy
     Ycurrent <- Y
     for (d in 1:N) {
+
+      ## stock bound every keep iterations
+      if (ite_cpt %% keep == 0)
+        bounds = c(bounds, current_bound)
 
       delta_bound = rep(0, Q) # lower bounds change induced by each possible swap
       lda_temp = list() # list of LDA models to stock the bound change for each swap
@@ -149,7 +181,7 @@ mmpca_clust <- function(dtm, Q, K,
           Pi_temp = csize_temp/N
 
           ## Temporary meta-observations. Only construct those impacted by the swap : Y[d] & q
-          P_temp = t(sparseMatrix(i = 1:N, j = Y_temp, x = rep(1,N)) )
+          P_temp = Matrix::t(Matrix::sparseMatrix(i = 1:N, j = Y_temp, x = rep(1,N)) )
           dtm_temp = slam::as.simple_triplet_matrix(P_temp[c(Y[d], q),] %*% DTMtoSparse(dtm))
 
           ## Update meta-bounds : temporary VE-step
@@ -171,7 +203,7 @@ mmpca_clust <- function(dtm, Q, K,
 
         if (best != Y[d]) { # apply the swap : document d goes into cluster best
 
-          ## M-step
+          ## M-step on $\pi$
           csize[best] = csize[best] + 1
           csize[Y[d]] = csize[Y[d]] - 1
           PI = csize/N
@@ -190,59 +222,58 @@ mmpca_clust <- function(dtm, Q, K,
           Y[d] = best
 
           ## Update meta-observations
-          P = t(sparseMatrix(i = 1:N, j = Y, x = rep(1,N)) )
+          P = Matrix::t(Matrix::sparseMatrix(i = 1:N, j = Y, x = rep(1,N)) )
           dtm_algo = slam::as.simple_triplet_matrix(P %*% DTMtoSparse(dtm))
 
           if (estimate.beta.algo == TRUE) {
-            # re-estimate Beta : unstable and long.
-            lda_algo = topicmodels::LDA(dtm_algo, model = lda_algo, control = control_lda_init, k = K)
-            lbeta  = t(lda_algo@beta)
+            ## re-estimate Beta on aggreated dtm at each swap : unstable and long.
+            lda_algo = topicmodels::LDA(dtm_algo,
+                                        k = K,
+                                        model = lda_algo,
+                                        control = control_lda_loop
+                                        )
           }
-
-
-
         }
       }else{
         if (verbose > 0) cat('\n Can\'t swap doc ', d, ' !\n')
       }
 
-      ## stock bound every keep iterations
       ite_cpt = ite_cpt + 1
-      if (ite_cpt %% keep) bounds = c(bounds, current_bound)
+
     }
 
-    if (identical(Ycurrent, Y)) {
-      #break of outer for loop (of the B&B)
+    #break of outer for loop (of the B&B)
+    if (identical(Ycurrent, Y))
       break
-    }
+
   }
 
   ###################################################################
   ## Re-estimate beta on the aggregated DTM at the end of B&B ---
   ###################################################################
 
-  lda_algo = topicmodels::LDA(dtm_algo,
-                 k = K,
-                 method = 'VEM',
-                 model = lda_algo,
-                 control = list(estimate.alpha = FALSE,
-                                estimate.beta = TRUE,
-                                alpha = alpha,
-                                verbose = 0,
-                                nstart = 1,
-                                var = list(iter.max = 100, tol = fixedpoint.tol)                                                                               )
-                 )
-  lbeta = t(lda_algo@beta)
+  # lda_algo = topicmodels::LDA(dtm_algo,
+  #                k = K,
+  #                method = 'VEM',
+  #                model = lda_algo,
+  #                control = list(estimate.alpha = FALSE,
+  #                               estimate.beta = TRUE,
+  #                               alpha = alpha,
+  #                               verbose = 0,
+  #                               nstart = 1,
+  #                               var = list(iter.max = 100)                                                                               )
+  #                )
+
+  beta <- exp(lda_algo@beta) / rowSums(exp(lda_algo@beta))
+
 
   ## Check for conservation of sum(Vgammas)
-  if (debug) {
-    Vgamma = computeVgamma(lda = lda_algo, dtm.aggr = dtm_algo)
-    gammaSum = sum(Vgamma)
-    if (gammaSum != theoretical.gammaSum )
-      warning('Problem : total sum of pseudo counts not conserved !')
-  }
-
-
+  # if (debug) {
+  #   Vgamma = computeVgamma(lda = lda_algo, dtm.aggr = dtm_algo)
+  #   gammaSum = sum(Vgamma)
+  #   if (gammaSum != theoretical.gammaSum )
+  #     warning('Problem : total sum of pseudo counts not conserved !')
+  # }
 
   #######################
   # Compute final bound
@@ -255,7 +286,7 @@ mmpca_clust <- function(dtm, Q, K,
   ###########################
 
 
-  P = t(sparseMatrix(i = 1:N, j = Y, x = rep(1,N)) )
+  P = Matrix::t(Matrix::sparseMatrix(i = 1:N, j = Y, x = rep(1,N)) )
   dtm_final = slam::as.simple_triplet_matrix(P %*% DTMtoSparse(dtm))
 
   b <- topicmodels::dtm2ldaformat(dtm_final)
@@ -271,22 +302,33 @@ mmpca_clust <- function(dtm, Q, K,
     Vgamma_final[q,] = Egama[q,]*(alpha*K + Nq[q])
   }
 
-  return(list(Yinit = Yinit,
-              init_method = init_method,
-              Y = Y,
-              lda_init = lda_init,
-              lda_final = lda_algo,
-              final_bound = final_bound,
-              bounds = bounds ,
-              ARIs = ARIs,
-              gamma = Vgamma_final,
-              max.iter = max.iter,
-              n_iterations = ite,
-              alpha = alpha,
-              lda_controls = list(init = control_lda_init, loop = control_lda_loop),
-              Q = Q,
-              K = K)
-         )
+  Vgamma_final <- Vgamma_final / rowSums(Vgamma_final)
+
+  ########################
+  # Compute ICL
+  ########################
+  icl <- final_bound - ((K * (V - 1))) - (Q - 1) - Q * (K - 1)
+
+  res <- new("mmpca_clust",
+            # call = mycall,
+            method = method,
+            Yinit = Yinit,
+            clustering = Y,
+            K = as.integer(K),
+            Q = as.integer(Q),
+            N = N,
+            V = V,
+            beta = beta,
+            gamma = Vgamma_final,
+            lda_algo = lda_algo,
+            max.epochs = as.integer(max.epochs),
+            logLikelihoods = bounds,
+            n_epochs = epoch,
+            llhood = final_bound,
+            icl = icl
+            )
+
+  res
 }
 
 
