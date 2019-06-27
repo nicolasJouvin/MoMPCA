30618d326f6e94652c3463b3a1919e08216478ef
nicolasJouvin <nicolasJouvin>
2019-06-27 19:36:20 +0200

    package doc

---
+++ R/MMPCA.R
@@ -0,0 +1,11 @@
+#' MMPCA: Clustering for counts data
+#'
+#' The MMPCA package enables the clustering of counts data such as document/term matrix with
+#' the mixture of multinomial PCA model. Model selection is performed via an
+#' approximated form of the Integrated Classification Likelihood. The clustering
+#' is done via the branch & bound described in the paper of ???.
+#'
+#'
+#' @docType package
+#' @name MMPCA
+NULL
