f9b72e740b595eb6e118b95d6cbdc1911b00d2c7
nicolasJouvin <nicolasJouvin>
2019-06-27 19:36:37 +0200

    BBCmsg doc

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' BBC articles
+#'
+#' Pre-procecessed BBC articles from the BBC news network.
+#' @docType data
+#'
+#' @usage data("BBCmsg")
+#'
+#' @format A list of 4 character vectors containing the vectorized and stemmed documents
+#' (i.e., unigrams with repetition) ;
+#'  \describe{
+#'   \item{msg1}{the birth of princess Charlotte}
+#'   \item{msg2}{black holes in astrophysics}
+#'   \item{msg3}{UK politics}
+#'   \item{msg4}{cancer diseases in medicine}
+#' }
+#'
+"BBCmsg"
